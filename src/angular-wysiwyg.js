b24514f1049ea939e742deca1fc73a97e8fec08b
Bruno Lerín <bruno@abstra.cc>
2016-09-07 12:01:55 +0200

    Prepare for minifying

--- src/angular-wysiwyg.js
+++ src/angular-wysiwyg.js
@@ -4,25 +4,25 @@ Usage: <wysiwyg textarea-id="question" textarea-class="form-control"  textarea-h
         textarea-id             The id to assign to the editable div
         textarea-class          The class(es) to assign to the the editable div
         textarea-height         If not specified in a text-area class then the hight of the editable div (default: 80px)
-        textarea-name           The name attribute of the editable div 
+        textarea-name           The name attribute of the editable div
         textarea-required       HTML/AngularJS required validation
         textarea-menu           Array of Arrays that contain the groups of buttons to show Defualt:Show all button groups
         ng-model                The angular data model
-        enable-bootstrap-title  True/False whether or not to show the button hover title styled with bootstrap  
+        enable-bootstrap-title  True/False whether or not to show the button hover title styled with bootstrap
 
-Requires: 
+Requires:
     Twitter-bootstrap, fontawesome, jquery, angularjs, bootstrap-color-picker (https://github.com/buberdds/angular-bootstrap-colorpicker)
 
 */
 
 /*
-    TODO: 
+    TODO:
         tab support
         custom button fuctions
 
         limit use of scope
         use compile fuction instead of $compile
-        move button elements to js objects and use doc fragments 
+        move button elements to js objects and use doc fragments
 */
 
 (function(angular, undefined) {
@@ -43,7 +43,7 @@ Requires:
     ];
 
     angular.module('wysiwyg.module', ['colorpicker.module'])
-        .directive('wysiwyg', function($timeout, wysiwgGui, $compile) {
+        .directive('wysiwyg', ['$timeout', 'wysiwgGui', '$compile', function($timeout, wysiwgGui, $compile) {
             return {
                 template: '<div>' +
                     '<style>' +
@@ -223,7 +223,7 @@ Requires:
                         }
 
                         ngModelController.$setViewValue(html);
-                    }); 
+                    });
 
                     textarea.on('keydown', function(event){
                         if (event.keyCode == 9){
@@ -231,11 +231,11 @@ Requires:
                             var html = textarea.html();
                             var selection = window.getSelection();
                             var position = selection.anchorOffset;
-                    
+
                             event.preventDefault();
                             // html = insertTab(html, position);
                             // textarea.html(html);
-                            // selection.collapse(textarea[0].firstChild, position + TAB_SPACES);    
+                            // selection.collapse(textarea[0].firstChild, position + TAB_SPACES);
                         }
                     });
 
@@ -246,7 +246,7 @@ Requires:
                             scope.isStrikethrough = scope.cmdState('strikethrough');
                             scope.isItalic = scope.cmdState('italic');
                             scope.isSuperscript = itemIs('SUP'); //scope.cmdState('superscript');
-                            scope.isSubscript = itemIs('SUB'); //scope.cmdState('subscript');    
+                            scope.isSubscript = itemIs('SUB'); //scope.cmdState('subscript');
                             scope.isRightJustified = scope.cmdState('justifyright');
                             scope.isLeftJustified = scope.cmdState('justifyleft');
                             scope.isCenterJustified = scope.cmdState('justifycenter');
@@ -384,8 +384,8 @@ Requires:
                 scope.format('enableobjectresizing', true);
                 scope.format('styleWithCSS', true);
             }
-        })
-        .factory('wysiwgGui', function(wysiwgGuiElements) {
+        }])
+        .factory('wysiwgGui', ['wysiwgGuiElements', function(wysiwgGuiElements) {
 
             var ELEMENTS = wysiwgGuiElements;
             var custom = {};
@@ -489,7 +489,7 @@ Requires:
                 setCustomElements: setCustomElements
             };
 
-        })
+        }])
         .value('wysiwgGuiElements', {
             'bold': {
                 tag: 'button',
